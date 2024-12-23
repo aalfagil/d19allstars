c7ad90f08d55bc796197c93cf958a71f32a73ff5
Mohammad Gilang Al Faritsy <alfagil@192.168.1.5>
2024-12-23 22:58:17 +0700

    push assets

---
+++ js/countdown-custom.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2025, 02, 15, 10)}); // year, month, date, hour
+            // setTimeout(function() {
+            //     $('#defaultCountdown').countdown('pause');
+            // }, 2000
+
+            //     )
+
+        });
+});		
+
